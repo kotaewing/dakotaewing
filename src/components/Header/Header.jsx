c2c7490e99e23ca288f61c41b86a794d2f000dee
kotaewing <kota.ewing@gmail.com>
2021-08-22 11:21:26 -0600

    getting rid of unused variables

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,9 @@ function Header(props) {
     return (
     <header>
         <Nav 
-        renderAbout={props.renderAbout}
         setRenderAbout={props.setRenderAbout}
-        renderPortfolio={props.renderPortfolio}
         setRenderPortfolio={props.setRenderPortfolio}
-        renderContact={props.renderContact}
         setRenderContact={props.setRenderContact}
-        renderResume={props.renderResume}
         setRenderResume={props.setRenderResume}
         />
     </header>
