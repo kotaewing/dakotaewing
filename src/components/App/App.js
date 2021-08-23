ee7b41e2e39e5c76e59710786df4906c13e575d8
kotaewing <kota.ewing@gmail.com>
2021-08-22 18:33:41 -0600

    changed order of projects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,24 +9,6 @@ import Resume from '../Resume';
 function App() {
   const [projectArray] = useState([
     {
-      title: 'My Garden',
-      link: 'https://kotaewing.github.io/my-garden/',
-      github: 'https://github.com/kotaewing/my-garden',
-      thumbnail: 'my-garden.png'
-    },
-    {
-      title: 'Weather Dashboard',
-      link: 'https://kotaewing.github.io/my-weather-dashboard/',
-      github: 'https://github.com/kotaewing/my-weather-dashboard',
-      thumbnail: 'weather-dashboard.png'
-    },
-    {
-      title: 'book-search',
-      link: 'https://dke-book-search.herokuapp.com/',
-      github: 'https://github.com/kotaewing/book-search',
-      thumbnail: 'book-search.png'
-    },
-    {
       title: 'On The Road',
       link: 'https://ontheroadapp.herokuapp.com/',
       github: 'https://github.com/kotaewing/OnTheRoad',
@@ -37,6 +19,24 @@ function App() {
       link: 'https://painter1y.herokuapp.com/',
       github: 'https://github.com/kotaewing/painterly',
       thumbnail: 'painterly.png'
+    },
+    {
+      title: 'My Garden',
+      link: 'https://kotaewing.github.io/my-garden/',
+      github: 'https://github.com/kotaewing/my-garden',
+      thumbnail: 'my-garden.png'
+    },
+    {
+      title: 'book-search',
+      link: 'https://dke-book-search.herokuapp.com/',
+      github: 'https://github.com/kotaewing/book-search',
+      thumbnail: 'book-search.png'
+    },
+    {
+      title: 'Weather Dashboard',
+      link: 'https://kotaewing.github.io/my-weather-dashboard/',
+      github: 'https://github.com/kotaewing/my-weather-dashboard',
+      thumbnail: 'weather-dashboard.png'
     },
     {
       title: 'Coding Quiz',
