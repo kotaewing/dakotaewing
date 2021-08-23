11794436d8af325e54ca0411e694dd9a4cb98d6b
kotaewing <kota.ewing@gmail.com>
2021-08-22 18:32:13 -0600

    added downloadable resume

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -1,9 +1,68 @@
-function Portfolio() {
-    return (
-        <div>
-            Resume
-        </div>
-    )
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+
+const BootstrapButton = withStyles({
+  root: {
+    boxShadow: 'none',
+    textTransform: 'none',
+    fontSize: 16,
+    padding: '6px 12px',
+    border: '1px solid',
+    lineHeight: 1.5,
+    backgroundColor: 'black',
+    borderColor: 'black',
+    color: 'white',
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      '"Segoe UI"',
+      'Roboto',
+      '"Helvetica Neue"',
+      'Arial',
+      'sans-serif',
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"',
+    ].join(','),
+    '&:hover': {
+      backgroundColor: '#0069d9',
+      borderColor: '#0062cc',
+      boxShadow: 'none',
+    },
+    '&:active': {
+      boxShadow: 'none',
+      backgroundColor: '#0062cc',
+      borderColor: '#005cbf',
+    },
+    '&:focus': {
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.5)',
+    },
+  },
+})(Button);
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+}));
+
+
+function Resume() {
+  const classes = useStyles();
+
+  return (
+    <div>
+        <Grid container direction='row' justifyContent='center' alignItems='center'>
+            <Grid item xs>
+                <BootstrapButton href={require(`../../dist/Dakota_Ewing_-_Front_End_DeveloperWeb_Developer.pdf`).default} download variant="contained" color="primary" disableRipple className={classes.margin}>
+                    DOWNLOAD RESUME
+                </BootstrapButton>
+            </Grid>
+        </Grid>
+    </div>
+  );
 }
 
-export default Portfolio;
+export default Resume;
